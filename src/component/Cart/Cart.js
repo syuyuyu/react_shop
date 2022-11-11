5470f0d851f98c63842c4d80b365ca5b3c732c56
Syuan Liu <syuan.haha@gmail.com>
2022-11-11 15:45:45 +0800

    Update Cart

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {CartList} from './cart_data.js'
 
-export default function Cart(){
-  const products = CartList.map( product=>
-    <div className="cart-item">
+
+//方法一 使用prop傳值
+function RenderItem(product){
+  return(
+    <div className="cart-item" key={product.id}>
       <div className="cart-img">
         <img src={product.img} alt="img" />
       </div>
@@ -20,13 +22,23 @@ export default function Cart(){
       </div>
     </div>
   )
+}
 
+export default function Cart(){
   return (
     <>
     <div className="cart-container">
       <h3>購物籃</h3>
       <div className='cart-group'>
-        {products}
+        {CartList.map( product=>
+          <RenderItem
+          key= {product.id}
+          name={product.name}
+          img={product.img}
+          price={product.price}
+          quantity={product.quantity}
+          />
+        )}
       </div>
       <div className="cart-down cart-fee">
         <span>運費</span>
@@ -40,3 +52,44 @@ export default function Cart(){
     </>
   )
 }
+
+//方法二 將CartList.map單獨拉出來return
+// export default function Cart(){
+  // const products = CartList.map( product=>
+  //   <div className="cart-item" key={products.id}>
+  //     <div className="cart-img">
+  //       <img src={product.img} alt="img" />
+  //     </div>
+  //     <div className="cart-content">
+  //       <div className="cart-content-title">
+  //         <div key={product.id}>{product.name}</div>
+  //         <div>${product.price}</div>
+  //       </div>
+  //       <div className="cart-content-count">
+  //         <div className="cart-content-btn cart-content-btn-minus">-</div>
+  //         <div className="cart-content-num">{product.quantity}</div>
+  //         <div className="cart-content-btn cart-content-btn-add">+</div>
+  //       </div>
+  //     </div>
+  //   </div>
+  // )
+
+//   return (
+//     <>
+//     <div className="cart-container">
+//       <h3>購物籃</h3>
+//       <div className='cart-group'>
+//         {products}
+//       </div>
+//       <div className="cart-down cart-fee">
+//         <span>運費</span>
+//         <span>免費</span>
+//       </div>
+//       <div className="cart-down cart-total">
+//         <div>小計</div>
+//         <div>$400</div>
+//       </div>
+//     </div>
+//     </>
+//   )
+// }
