e965f25f09748f7e5a5553bafff6d5539357ac4b
Syuan Liu <syuan.haha@gmail.com>
2022-11-11 15:23:16 +0800

    Update sample.img

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -1,36 +1,32 @@
 import React from 'react';
 import {CartList} from './cart_data.js'
 
-
 export default function Cart(){
   const products = CartList.map( product=>
     <div className="cart-item">
-        <div className="cart-img"><img src={product.img} alt="img" /></div>
-        <div className="cart-content">
-          <div className="cart-content-title">
-            <div key={product.id}>{product.name}</div>
-            <div>${product.price}</div>
-          </div>
-          <div className="cart-content-count">
-            <div className="cart-content-btn cart-content-btn-minus">-</div>
-            <div className="cart-content-num">{product.quantity}</div>
-            <div className="cart-content-btn cart-content-btn-add">+</div>
-          </div>
+      <div className="cart-img">
+        <img src={product.img} alt="img" />
+      </div>
+      <div className="cart-content">
+        <div className="cart-content-title">
+          <div key={product.id}>{product.name}</div>
+          <div>${product.price}</div>
+        </div>
+        <div className="cart-content-count">
+          <div className="cart-content-btn cart-content-btn-minus">-</div>
+          <div className="cart-content-num">{product.quantity}</div>
+          <div className="cart-content-btn cart-content-btn-add">+</div>
         </div>
       </div>
-    )
-    
-    
-
+    </div>
+  )
 
   return (
     <>
     <div className="cart-container">
       <h3>購物籃</h3>
       <div className='cart-group'>
-          {/* 變數從我開始cart-item */}
         {products}
-        {/* cart item */}
       </div>
       <div className="cart-down cart-fee">
         <span>運費</span>
